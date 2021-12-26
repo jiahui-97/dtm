dd01e567b7170d5250011a54bc72583839d6e8b8
yedf2 <120050102@qq.com>
2021-12-26 17:44:38 +0800

    update bench scripts

--- bench/prepare-bolt.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-curl https://raw.githubusercontent.com/dtm-labs/dtm/alpha/bench/setup.sh -o setup.sh
-sh setup.sh
-
-echo 'all prepared. you shoud run following commands to test in different terminal'
-echo
-echo 'cd dtm && go run bench/main.go boltdb'
-echo 'cd dtm && bench/run-boltdb.sh'
-
--- bench/prepare-redis.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-curl https://raw.githubusercontent.com/dtm-labs/dtm/alpha/bench/setup.sh -o setup.sh
-sh setup.sh
-
-echo 'all prepared. you shoud run following commands to test in different terminal'
-echo
-echo 'cd dtm && go run bench/main.go redis'
-echo 'cd dtm && bench/run-redis.sh'
